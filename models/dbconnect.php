bae0346103c803d5f6100527d6786eb2367090da
isador999 <A103@yopmail.com>
2016-07-29 19:10:45 +0200

    Update dbconnect.php

--- models/dbconnect.php
+++ models/dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-	$DB = new PDO('mysql:host=localhost;dbname=gozpeak_dev', 'gozpeak_dev', '#gozpeak_dev_pass#');
+	$DB = new PDO('mysql:host=YOUR_HOST;dbname=YOUR_DB', 'YOUR_USER', 'YOUR_PASS');
 	$DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$DB->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 	$DB->exec("SET CHARACTER SET utf8");
